73c440cf17bab9ccf7abfcc9fd885867340aed1b
arindam9999 <45881005+arindam9999@users.noreply.github.com>
2020-09-29 16:55:19 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-BASE = "http://127.0.0.1:5000/"
-files = {'image': open('image.png', 'rb')}
-
-response = requests.get(BASE+"color_picker", files=files)
-
-print(response.json())
